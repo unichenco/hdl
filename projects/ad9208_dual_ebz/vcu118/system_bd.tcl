826bbd4f4a045b00a992393071c80fb6a3f503f5
Laszlo Nagy <laszlo.nagy@analog.com>
2019-04-04 16:16:11 +0100

    ad9208_dual_ebz: Initial version
    This commit add support for the dual AD9208-DUAL-EBZ board.
    
    The clocking scheme is different from the other projects.
    The device clock (LaneRate/40) is no longer an output of the transceivers (RXOUTCLOCK),
    it is received directly from the clockchip  SCLKOUT9 output through the REFCLK1.
    This is needed for deterministic latency where SYSREF must be sampled
    with the device clock by meeting setup and hold time.
    
    The two channels from each converter are merged together and transferred  to the DDR with a single DMA.
    
    It has all transceiver parameters set for a 15Gpbs lane rate and uses the QPLL.

---
+++ projects/ad9208_dual_ebz/vcu118/system_bd.tcl
@@ -0,0 +1,38 @@
+
+## FIFO depth is 4Mb - 250k samples (65k samples per converter)
+set adc_fifo_address_width 13
+
+source $ad_hdl_dir/projects/common/vcu118/vcu118_system_bd.tcl
+source $ad_hdl_dir/projects/common/xilinx/adcfifo_bd.tcl
+source ../common/dual_ad9208_bd.tcl
+
+foreach i {0 1} {
+
+  ad_ip_parameter util_adc_${i}_xcvr CONFIG.RX_CLK25_DIV 30
+  ad_ip_parameter util_adc_${i}_xcvr CONFIG.CPLL_CFG0 0x1fa
+  ad_ip_parameter util_adc_${i}_xcvr CONFIG.CPLL_CFG1 0x2b
+  ad_ip_parameter util_adc_${i}_xcvr CONFIG.CPLL_CFG2 0x2
+  ad_ip_parameter util_adc_${i}_xcvr CONFIG.CPLL_FBDIV 2
+  #Missing CHANNEL params:
+  # CH_HSPMUX                     0b100000001000000
+  # PREIQ_FREQ_BST                1
+  # RTX_BUF_CML_CTRL              0b101
+  # RXPI_CFG0                     0b11000000000010
+
+  ad_ip_parameter util_adc_${i}_xcvr CONFIG.QPLL_REFCLK_DIV 1
+  ad_ip_parameter util_adc_${i}_xcvr CONFIG.QPLL_CFG0 0x333c
+  ad_ip_parameter util_adc_${i}_xcvr CONFIG.QPLL_CFG4 0x2
+  ad_ip_parameter util_adc_${i}_xcvr CONFIG.QPLL_FBDIV 20
+  #Missing COMMON params:
+  # PPF0_CFG               0b101100000000
+  # QPLL0_LPF              0b1101111111
+}
+
+
+# Set the smart interconnect to use a lower speed switch to meet timing
+set_property -dict [list CONFIG.ADVANCED_PROPERTIES {  __view__ { \
+    timing { M00_Buffer { AR_SLR_PIPE 1 AW_SLR_PIPE 1 B_SLR_PIPE 1 R_SLR_PIPE 1 W_SLR_PIPE 1 } } \
+    clocking { SW0 { ASSOCIATED_CLK aclk1 } } \
+  } }] [get_bd_cells axi_mem_interconnect]
+
+
