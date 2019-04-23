wueesmic/ad-ipfn-hdl

util_adxcvr: gty4 params for 15Gbps link

library/xilinx/util_adxcvr/util_adxcvr_xch.v
@@ -2454,7 +2454,7 @@ module util_adxcvr_xch #(
       .CHAN_BOND_SEQ_2_ENABLE       (4'b1111),
       .CHAN_BOND_SEQ_2_USE          ("FALSE"),
       .CHAN_BOND_SEQ_LEN            (1),
-      .CH_HSPMUX                    (16'b0010000000100000),
+      .CH_HSPMUX                    (16'b100000001000000),
       .CKCAL1_CFG_0                 (16'b1100000011000000),
       .CKCAL1_CFG_1                 (16'b0001000011000000),
       .CKCAL1_CFG_2                 (16'b0010000000001000),
@@ -2583,11 +2583,11 @@ module util_adxcvr_xch #(
       .PD_TRANS_TIME_FROM_P2        (12'b000000111100),
       .PD_TRANS_TIME_NONE_P2        (8'b00011001),
       .PD_TRANS_TIME_TO_P2          (8'b01100100),
-      .PREIQ_FREQ_BST               (0),
+      .PREIQ_FREQ_BST               (1),
       .RATE_SW_USE_DRP              (1'b1),
       .RCLK_SIPO_DLY_ENB            (1'b0),
       .RCLK_SIPO_INV_EN             (1'b0),
-      .RTX_BUF_CML_CTRL             (3'b100),
+      .RTX_BUF_CML_CTRL             (3'b101),
       .RTX_BUF_TERM_CTRL            (2'b00),
       .RXBUFRESET_TIME              (5'b00011),
       .RXBUF_ADDR_MODE              ("FAST"),
@@ -2707,7 +2707,7 @@ module util_adxcvr_xch #(
       .RXPHSAMP_CFG                 (16'b0010000100000000),
       .RXPHSLIP_CFG                 (16'b1001100100110011),
       .RXPH_MONITOR_SEL             (5'b00000),
-      .RXPI_CFG0                    (16'b0000000100000010),
+      .RXPI_CFG0                    (16'b11000000000010),
       .RXPI_CFG1                    (16'b0000000001010100),
       .RXPMACLK_SEL                 ("DATA"),
       .RXPMARESET_TIME              (5'b00011),

unichenco/hdl

library/xilinx/util_adxcvr/util_adxcvr_xch.v
@@ -2459,7 +2459,7 @@ module util_adxcvr_xch #(
       .CHAN_BOND_SEQ_2_ENABLE       (4'b1111),
       .CHAN_BOND_SEQ_2_USE          ("FALSE"),
       .CHAN_BOND_SEQ_LEN            (1),
-      .CH_HSPMUX                    (16'b0010000000100000),
+      .CH_HSPMUX                    (16'b100000001000000),
       .CKCAL1_CFG_0                 (16'b1100000011000000),
       .CKCAL1_CFG_1                 (16'b0001000011000000),
       .CKCAL1_CFG_2                 (16'b0010000000001000),
@@ -2588,11 +2588,11 @@ module util_adxcvr_xch #(
       .PD_TRANS_TIME_FROM_P2        (12'b000000111100),
       .PD_TRANS_TIME_NONE_P2        (8'b00011001),
       .PD_TRANS_TIME_TO_P2          (8'b01100100),
-      .PREIQ_FREQ_BST               (0),
+      .PREIQ_FREQ_BST               (1),
       .RATE_SW_USE_DRP              (1'b1),
       .RCLK_SIPO_DLY_ENB            (1'b0),
       .RCLK_SIPO_INV_EN             (1'b0),
-      .RTX_BUF_CML_CTRL             (3'b011),
+      .RTX_BUF_CML_CTRL             (3'b101),
       .RTX_BUF_TERM_CTRL            (2'b00),
       .RXBUFRESET_TIME              (5'b00011),
       .RXBUF_ADDR_MODE              ("FAST"),
@@ -2712,7 +2712,7 @@ module util_adxcvr_xch #(
       .RXPHSAMP_CFG                 (16'b0010000100000000),
       .RXPHSLIP_CFG                 (16'b1001100100110011),
       .RXPH_MONITOR_SEL             (5'b00000),
-      .RXPI_CFG0                    (16'b0000000100000000),
+      .RXPI_CFG0                    (16'b0011000000000010),
       .RXPI_CFG1                    (16'b0000000001010100),
       .RXPMACLK_SEL                 ("DATA"),
       .RXPMARESET_TIME              (5'b00011),